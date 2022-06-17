95a08c3d4fae26bf3cad257807c088406d3e583c
Bruce Johnson <johnswanson3.9@gmail.com>
2022-06-17 13:10:33 -0600

    adds styling to LandingScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { LandingScreen } from './LandingScreen';
+import tw from './tailwind';
 
 export const App: React.FC = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw`flex-1`}>
       <LandingScreen />
     </SafeAreaView>
   );
--- src/LandingScreen/LandingScreen.tsx
+++ src/LandingScreen/LandingScreen.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import tw from '../tailwind';
-import { Text } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export const LandingScreen: React.FC = () => (
   <>
-    <Text style={tw`text-2xl text-darker-green`}>WhatReads</Text>
+    <View style={tw`flex-1 items-center justify-center mt-60`}>
+      <View style={tw`bg-tannish p-16px rounded-6`}>
+        <Text style={tw`text-4xl text-darker-green font-bold`}>WhatReads</Text>
+      </View>
+      <View style={tw`w-50 items-center mt-5`}>
+        <Text style={tw`flex-wrap justify-evenly content-around text-[20px]`}>
+          ... humans do!
+        </Text>
+      </View>
+    </View>
+    <View style={tw`flex-1 items-center justify-center mt-7`}>
+      <Pressable style={tw`p-12px rounded-4 bg-pale-green`}>
+        <Text style={tw`text-xl tracking-wide`}>Continue</Text>
+      </Pressable>
+    </View>
   </>
 );
