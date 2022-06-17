991e8a9c303ef3b987825e7210f65d2d372f4d8e
Bruce Johnson <johnswanson3.9@gmail.com>
2022-06-17 09:44:18 -0600

    adds tailwind configuration and LandingScreen

---
+++ src/LandingScreen/LandingScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import tw from '../tailwind';
+import { Text } from 'react-native';
+
+export const LandingScreen: React.FC = () => (
+  <>
+    <Text style={tw`text-2xl text-darker-green`}>WhatReads</Text>
+  </>
+);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    colors: {
+      'darker-green': '#6A8D73',
+      'pale-green': '#E4FFE1',
+      'green-yellow': '#F4FDD9',
+      tannish: '#FFE8C2',
+      'orange-dust': '#F0A868',
+    },
+  },
+};
